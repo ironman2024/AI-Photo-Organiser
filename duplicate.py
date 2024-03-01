cc597279ae120abc138aff9285e7e7b5b7fdb0b8
shwetasingole <115386998+shwetasingole@users.noreply.github.com>
2024-02-29 22:34:05 -0800

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -1,1 +1,11 @@
+from imutils import paths
+import numpy as np
+import argparse
+import cv2
+import os
 
+def dhash(image,hashSize=8):
+    gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    resized= cv2.resize(gray,(hashSize+1,hashSize))
+    diff =resized[:,1:]>resized[:,:-1]
+    return sum([2 **i for (i,v) in enumerate(diff.flatten()) if v])
