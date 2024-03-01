ba6fa56c365bc3420e960804b5407d50a9722be8
shwetasingole <115386998+shwetasingole@users.noreply.github.com>
2024-03-01 04:39:51 -0800

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -1,10 +1,13 @@
 from PIL import Image
 import imagehash
 import os
-main_folder=r"data_path"
+main_folder=r"path"
 images_folder=os.path.join(main_folder,'images_dataset')
+dhash_threshold=5
+
 images=[f for f in os.listdir(images_folder)if os.path.isfile(os.path.join(images_folder,f))]
-images.sort()
+
+duplicate_count=0
 
 for i in range(len(images)):
    image_path= os.path.join(images_folder,images[i])
@@ -15,5 +18,7 @@ for i in range(len(images)):
         image_path= os.path.join(images_folder,images[j])
         image=Image.open(image_path)
         hash_value_j=imagehash.dhash(image)
-        if(hash_value_j==hash_value_i):
-           print(f"Duplicate image Found:    {images[i]} | {images[j]}")
+        if abs(hash_value_j-hash_value_i) <= dhash_threshold:
+                print(f"Duplicate image Found using dhash:    {images[i]} | {images[j]}")
+                duplicate_count+=1
+print(f"Total number of duplicate counts are: {duplicate_count}")
