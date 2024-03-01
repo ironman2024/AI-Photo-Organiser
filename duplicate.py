4dd9c65ea500cdc58b5f8903966cf5c7fca6f1f7
shwetasingole <115386998+shwetasingole@users.noreply.github.com>
2024-03-01 07:37:39 -0800

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -1,24 +1,26 @@
 from PIL import Image
 import imagehash
 import os
-main_folder=r"path"
-images_folder=os.path.join(main_folder,'images_dataset')
-dhash_threshold=5
 
-images=[f for f in os.listdir(images_folder)if os.path.isfile(os.path.join(images_folder,f))]
+def remove_duplicates(main_folder):
+    walker=os.walk(main_folder)
+    print(walker)
+    uniquefiles=dict()
 
-duplicate_count=0
+    for folder,sub_folders,files in walker:
+        for file in files:
+            filepath=os.path.join(folder,file)
+            image=Image.open(filepath)
+            image_hash=imagehash.dhash(image)
+            print(f" dhash value:{image_hash}")
 
-for i in range(len(images)):
-   image_path= os.path.join(images_folder,images[i])
-   image=Image.open(image_path)
-   hash_value_i=imagehash.dhash(image)
-   print(f"Image: {images[i]} | dhash value :{hash_value_i}")
-   for j in range(i+1,len(images)):
-        image_path= os.path.join(images_folder,images[j])
-        image=Image.open(image_path)
-        hash_value_j=imagehash.dhash(image)
-        if abs(hash_value_j-hash_value_i) <= dhash_threshold:
-                print(f"Duplicate image Found using dhash:    {images[i]} | {images[j]}")
-                duplicate_count+=1
-print(f"Total number of duplicate counts are: {duplicate_count}")
+            if image_hash  in uniquefiles:
+               
+                os.remove(filepath)
+                print(f"{filepath} has been deleted")
+                
+            else:
+                uniquefiles[image_hash]=filepath
+            
+main_folder=r"your_path"
+remove_duplicates(main_folder)
