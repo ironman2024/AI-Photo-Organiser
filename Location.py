4d682527ee942fea52e73e32272aa86680ccd7f3
Atharva Karval <113116636+ironman2024@users.noreply.github.com>
2024-03-02 11:03:15 +0530

    Update Location.py

--- Location.py
+++ Location.py
@@ -1,16 +1,20 @@
+import sys
 import os
 import exifread
 from datetime import datetime
 from PIL import Image
+import shutil
 
 def get_image_metadata(image_path):
     try:
         with open(image_path, 'rb') as f:
+            # Read the EXIF tags
             tags = exifread.process_file(f)
 
+            # Extract GPS coordinates if available
             gps_latitude = tags.get('GPS GPSLatitude')
             gps_longitude = tags.get('GPS GPSLongitude')
-            
+
             if gps_latitude and gps_longitude:
                 lat_degrees = gps_latitude.values[0].num / gps_latitude.values[0].den
                 lat_minutes = gps_latitude.values[1].num / gps_latitude.values[1].den
@@ -25,6 +29,7 @@ def get_image_metadata(image_path):
                 gps_location = (latitude, longitude)
             else:
                 gps_location = None
+
             date_time = tags.get('EXIF DateTimeOriginal')
             if date_time:
                 date_time_str = str(date_time)
@@ -46,25 +51,25 @@ def process_files_in_directory(source_directory, destination_directory, no_metad
             gps_location, day_date = get_image_metadata(file_path)
 
             if day_date and gps_location:
+                # Create directories based on date and location
                 date_folder = os.path.join(destination_directory, day_date)
                 location_folder = os.path.join(date_folder, f"{gps_location[0]}, {gps_location[1]}")
 
                 os.makedirs(location_folder, exist_ok=True)
 
-                new_file_path = os.path.join(location_folder, filename)
-                os.rename(file_path, new_file_path)
+                # Copy the file to the location-based folder
+                shutil.copy(file_path, location_folder)
             else:
-
+                # Copy files without metadata to another directory
                 no_metadata_folder = os.path.join(no_metadata_directory, "NoMetadata")
                 os.makedirs(no_metadata_folder, exist_ok=True)
 
-                new_file_path = os.path.join(no_metadata_folder, filename)
-                os.rename(file_path, new_file_path)
+                shutil.copy(file_path, no_metadata_folder)
 
-source_directory = r"D:\iphone"
+source_directory = r"Images"
 
-destination_directory = r"D:\SortedPhotos"
+destination_directory = r"Location"
 
-no_metadata_directory = r"D:\NoMetadataPhotos"
+no_metadata_directory = r"Location"
 
 process_files_in_directory(source_directory, destination_directory, no_metadata_directory)
